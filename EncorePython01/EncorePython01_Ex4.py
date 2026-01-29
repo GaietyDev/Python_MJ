b30059d6c4b0f6cc9ad218571fdcf0024db89fd1
GaietyDev <mjimenez5860@students.vbisd.org>
2026-01-29 14:16:21 -0500

    EP01 Done

--- EncorePython01/EncorePython01_Ex4.py
+++ EncorePython01/EncorePython01_Ex4.py
@@ -1,4 +1,9 @@
-for row in range(5):
-    for col in range(4):
-        print("%4d"%(row+col), end="")
+for row in range(10):
+    for rjust in range(14):
+         print(" ", end = " ")
+   
+    for col in range(10):
+        #print("%4d    "%((1+row)*(1+col)), end="")
+        output = (row + 1) * (col + 1)
+        print(str(output).rjust(4, ' '), end = " ")
     print()
--- EncorePython01/EncorePython01_Ex5.py
+++ EncorePython01/EncorePython01_Ex5.py
@@ -1,5 +1,18 @@
 num_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 for row in range(10):
-    for col in range(6):
-        print("%4d"%
+    for rjust in range(29):
+         print(" ", end = " ")
+         
+    for col in range(3):
+        if col == 0:
+            output = num_list[row]
+        elif col == 1:
+            number = num_list[row]
+            output = number * number
+        else:
+            number = num_list[row]
+            output = number * number * number
+            
+        print(str(output).rjust(6, ' '), end = " ")
+    print()
